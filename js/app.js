6c4e617bb6b5fe7cd3139a168642b34dc2d05b04
kp1129 <41562973+kp1129@users.noreply.github.com>
2018-11-25 12:36:08 -0700

    update app.js

--- js/app.js
+++ js/app.js
@@ -1,23 +1,59 @@
-
-const resetDisplay = () => document.getElementById('overlay').style.display = 'none';
-
+// Set selectors
+const buttons = document.getElementsByClassName('key');
 const startButton = document.getElementById('btn__reset');
-
 const keys = document.getElementById('qwerty');
-
 let newGame;
 
+// This function resets the display
+const resetDisplay = () => {
+    const scoreboard = document.getElementById('scoreboard').firstElementChild;
+    for(let i = 0; i < buttons.length; i++){
+        buttons[i].disabled = false;
+        buttons[i].classList.remove('chosen');
+    }
+    let j = 0;
+    let lives = "";
+    const li = '<li class="tries"><img src="images/liveHeart.png" alt="Heart Icon" height="35" width="30"></li>';
+    while(j < 5){
+        lives += li;
+        j++;
+    }
+    scoreboard.innerHTML = lives;
+    document.getElementById('overlay').style.display = 'none';
+}
+
+// This function visually marks buttons on the display keyboard and calls handleInteraction
+const markButton = (e) => {
+        const clickedLetter = e.target.innerText;
+        e.target.disabled = true;
+        e.target.classList.add('chosen');
+        return newGame.handleInteraction(clickedLetter);
+}
+
+// Reset display when Start Game is clicked
 startButton.addEventListener('click', function(e){
-  resetDisplay();
   newGame = new Game();
+  resetDisplay();
   newGame.startGame();
 });
 
+// This event listener responds to the virtual keyboard
+keys.addEventListener('click', function(e){
+    if(e.target.tagName === 'BUTTON'){
+        markButton(e);
+    }
+});
 
-const markButton = (e) => {
-  const clickedLetter = e.target.innerText;
-  e.target.disabled = true;
-  return newGame.handleInteraction(clickedLetter);
-}
-
-keys.addEventListener('click', markButton);
+// This event listener enables users to use their physical keyboard
+document.addEventListener('keydown', function(e){
+    const regex = /[a-z]/
+    if(regex.test(event.key)){
+        for(let i = 0; i < buttons.length; i++){
+            if(buttons[i].innerText === event.key){
+                buttons[i].disabled = true;
+                buttons[i].classList.add('chosen');
+                return newGame.handleInteraction(event.key);
+            }
+        }
+    }
+});
